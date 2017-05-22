029d0b9de8b75efe4a369207e1b7c797231a593b
Martin <martin.lippl@pueo.de>
2017-05-23 00:15:09 +0200

    Simplified single_img_features()

--- search_and_classify.py
+++ search_and_classify.py
@@ -61,52 +61,6 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
         return features
 
 
-#
-# Diese Funktion kann durch single_img_features() vereinfacht werden.  
-# Können wir ganz zum schluss machen!
-#
-
-def extract_features(imgs, color_space='RGB', spatial_size=(32, 32), hist_bins=32, orient=9, pix_per_cell=8, cell_per_block=2, hog_channel=0, spatial_feat=True, hist_feat=True, hog_feat=True):
-
-	features = []
-
-	for file in imgs:
-		file_features = []
-		image = mpimg.imread(file)
-
-		if color_space != 'RGB':
-			if color_space == 'HSV':
-				feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-			elif color_space == 'LUV':
-				feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-			elif color_space == 'HLS':
-				feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-			elif color_space == 'YUV':
-				feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-			elif color_space == 'YCrCb':
-				feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-		else: feature_image = np.copy(image)      
-
-		if spatial_feat == True:
-			spatial_features = bin_spatial(feature_image, size=spatial_size)
-			file_features.append(spatial_features)
-		if hist_feat == True:
-			hist_features = color_hist(feature_image, nbins=hist_bins)
-			file_features.append(hist_features)
-		if hog_feat == True:
-			if hog_channel == 'ALL':
-				hog_features = []
-				for channel in range(feature_image.shape[2]):
-					hog_features.append(get_hog_features(feature_image[:,:,channel], orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True))
-				hog_features = np.ravel(hog_features)        
-			else:
-				hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-
-			file_features.append(hog_features)
-		features.append(np.concatenate(file_features))
-
-	return features
-
 
 def single_img_features(img, color_space='RGB', spatial_size=(32,32), hist_bins=32, orient=9, pix_per_cell=8, cell_per_block=2, hog_channel=0, spatial_feat=True, hist_feat=True, hog_feat=True):
 	img_features=[]
@@ -145,6 +99,18 @@ def single_img_features(img, color_space='RGB', spatial_size=(32,32), hist_bins=
 
 	return np.concatenate(img_features)
 
+
+
+def extract_features(imgs, color_space='RGB', spatial_size=(32, 32), hist_bins=32, orient=9, pix_per_cell=8, cell_per_block=2, hog_channel=0, spatial_feat=True, hist_feat=True, hog_feat=True):
+
+	features = []
+
+	for file in imgs:
+		image = mpimg.imread(file)
+		single_features = single_img_features(image, color_space, spatial_size, hist_bins, orient, pix_per_cell, cell_per_block, hog_channel, spatial_feat, hist_feat, hog_feat)
+		features.append(single_features)
+
+	return features
 
 
 
